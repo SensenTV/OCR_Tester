247227df586f672f47cfb72983c75ec4297850e4
SensenTV <MalestGmbHundKo.KG@gmx.de>
2026-09-03 15:34:54 +0200

    XUnit Tests Initialisierung

---
+++ OCR_Tester.Tests/GroundTruthReaderTests.cs
@@ -0,0 +1,13 @@
+﻿namespace OCR_Tester.Tests;
+
+public class GroundTruthReaderTests
+{
+    // Enthält Unit Tests für das Einlesen und Validieren
+    // von Ground-Truth-Daten aus JSON-Dateien.
+
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
---
+++ OCR_Tester.Tests/ResultEvaluatorTests.cs
@@ -0,0 +1,12 @@
+﻿namespace OCR_Tester.Tests;
+
+public class ResultEvaluatorTests
+{
+    // Enthält Unit Tests zur Überprüfung der Bewertung und Aggregation
+    // von OCR-Ergebnissen anhand der Ground Truth.
+    [Fact]
+    public void Test3()
+    {
+        Assert.True(true);
+    }
+}
